ab70478753ebcc0ac88ebfc5a2e4087cc6cd9ce0
Gaurav Reddy Yerradoddi <gyerrad1@CS444-gyerrad1.pods.bu.int>
2020-09-23 20:37:21 -0400

    this contains code for solving formulas

--- submit/prj1-sol/src/spreadsheet.mjs
+++ submit/prj1-sol/src/spreadsheet.mjs
@@ -4,14 +4,22 @@ import { cellRefToCellId } from './util.mjs';
 
 //use for development only
 import { inspect } from 'util';
+import { exit } from 'process';
 
 export default class Spreadsheet {
 
   //factory method
   static async make() { return new Spreadsheet(); }
 
-  constructor() {
+  constructor() { // this constructor is for spreadsheet
     //@TODO
+    
+    this.sheet = {}
+    // let initalobj= {id:new_cell}
+
+    
+
+    //cell.id=this.id;
   }
 
   /** Set cell with id baseCellId to result of evaluating formula
@@ -23,13 +31,80 @@ export default class Spreadsheet {
    *  syntax error and `CIRCULAR_REF` for a circular reference
    *  and message property set to a suitable error message.
    */
-  async eval(baseCellId, expr) {
+  
+  async eval(baseCellId, expr) { 
     const updates = {};
     //@TODO
+    let temp_array = this.auxeaval(baseCellId, expr);
+    //exit()
+    updates[temp_array[0]]= temp_array[1];
+    
     return updates;
   }
 
+  
+
   //@TODO add methods
+   auxeaval(baseCellId, expr) {
+    let ast =parse(expr,baseCellId);
+    //console.log(ast);
+    console.log(inspect(ast, false, Infinity));
+    switch(ast.type){
+      case 'num':
+        return [baseCellId,ast.value]
+      
+      case 'app':
+          
+          if (ast.kids.length==1){
+            return [baseCellId,-ast.kids[0].value]
+          }
+          else{
+          this.parseASTapp(ast)
+          return [baseCellId,ast.value]
+          }
+          
+        
+        
+    }
+  }
+  array=[]
+
+  parseTwo(ast){
+    ast.kids.forEach(element=>{
+      if(element.type==='num'){
+        this.array.push(element.value)
+      }
+      else if(element.type==='app'){
+        this.array.push(element.fn)
+        this.parseTwo(element)
+      }
+
+    })
+
+  }
+
+  /*this fuction is ast with more than 
+  one kids and fns below is to use it*/
+   parseASTapp(ast) {
+    if (ast.type === 'num' || ast.kids.length === 0)
+        return;
+    ast.kids.forEach(a => this.parseASTapp(a));
+    let val = ast.kids.reduce((a,b) =>  this.FNS[ast.fn](a.value, b.value));
+    ast.kids = [];
+    ast.type = 'num';
+    ast.value = val;
+    delete ast.fn;
+}
+
+ FNS = {
+  '+': (a, b) => a + b,
+  '-': (a, b=null) => b === null ? -a : a - b ,
+  '*': (a, b) => a * b,
+  '/': (a, b) => a / b,
+  min: (...args) => Math.min(...args),
+  max: (...args) => Math.max(...args),
+}
+  
 }
 
 //Map fn property of Ast type === 'app' to corresponding function.
@@ -44,3 +119,15 @@ const FNS = {
 
 
 //@TODO add other classes, functions, constants etc as needed
+class Cellinfo{
+ 
+  
+  constructor(id,expr,value,dependents,ast){
+    this.id=id;
+    this.expr=expr;
+    this.value=value;
+    this.dependents=dependents;
+    this.ast=ast;
+  }
+ 
+}
