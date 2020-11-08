bd575218ecc0fafd8c27a2579931a13b3b037ef5
Gaurav-Reddy <gauravcloudiot@gmail.com>
2020-11-08 17:55:22 -0500

    trial 2

--- submit/prj4-sol/src/ss-server.mjs
+++ submit/prj4-sol/src/ss-server.mjs
@@ -41,7 +41,8 @@ export default function serve(port, store) {
 
 function setupRoutes(app) {
   app.use(bodyParser.urlencoded({extended: true}));
-  
+  app.get(`/`,indexLoad(app));
+  app.post(`/`,bodyParser.urlencoded({extended: false}),searchCells(app)); 
   //@TODO add routes
   //must be last
   app.use(do404(app));
@@ -49,11 +50,46 @@ function setupRoutes(app) {
 
 }
 
-//@TODO add handlers
+//@TODO add handlers 
 
 /** Default handler for when there is no route for a particular method
- *  and path.
+ *  and path. 
  */
+function searchCells(app){
+  return async function(req, res) {
+    
+    try{
+      const errorMsg={};
+      const isValid=validateField('ssName',req.body,errorMsg);
+      
+      
+      if(!isValid){
+        res.status(BAD_REQUEST).
+      send(app.locals.mustache.render('index',{ errorMessage: errorMsg['ssName'] }));
+      }
+      //-------------------------------------------->retrive from db
+      
+      
+      const viewData= await buildTable(req.body.ssName,app.locals.store);
+      console.log({table:viewData});
+      //-------------------------------------------->retrive from db END
+      res.status(OK).
+      send(app.locals.mustache.render('test',{table:viewData}));
+    }catch(err){
+      console.log(err);
+    }
+    
+   
+  };
+}
+
+function indexLoad(app){
+  return async function(req, res) {
+    
+    res.status(OK).
+      send(app.locals.mustache.render('index',{errorMessage:""}));
+  };
+}
 function do404(app) {
   return async function(req, res) {
     const message = `${req.method} not supported for ${req.originalUrl}`;
@@ -81,6 +117,60 @@ const MIN_ROWS = 10;
 const MIN_COLS = 10;
 
 //@TODO add functions to build a spreadsheet view suitable for mustache
+
+async function buildTable(name,store){
+  const sheet = await Spreadsheet.make(name,store);
+  const data=sheet.dump();
+  const cellids=[];
+  for(const id of data){cellids.push(id[0]);}
+  const rowsCols=getNoofRowsAndCols(cellids);
+  
+  
+  let dataArray=createEmptyArray(rowsCols[0],rowsCols[1]);  //create an empty array;will be filled with data
+  //dataArray.push(getFirstRow(name,rowsCols[0]));
+  if(data.length!=0){fillDataArray(cellids,dataArray,sheet);}             //populate with data
+  let dataJson=[];
+  let sNo=0;
+  for (const rowD of dataArray){
+    sNo++;
+    if(sNo===1){dataJson.push({row:getFirstRow(name,rowsCols[0]).join('')})}
+    dataJson.push({row:'<td><b>'+sNo+'</b></td>'+rowD.join('')});
+  }
+  
+  return dataJson;
+};
+function fillDataArray(array,dataArray,connector){
+  for(const i of array){
+    
+    const cellvalue=connector.query(i).value;
+    const rowNo = parseInt(i.substr(1))-1;
+    const colNo = i[0].charCodeAt(0)-96-1;
+    dataArray[rowNo][colNo]= '<td>'+cellvalue+'</td>';
+    
+  }
+}
+function createEmptyArray(rowsNo,colsNo){
+  return new Array(rowsNo).fill('<td>&nbsp;</td>').map(() => new Array(colsNo).fill('<td>&nbsp;</td>'));
+}
+function getFirstRow(name,rowsno){
+  const row=[];
+  for(let i=0;i<rowsno;i++){
+    row.push('<td><b>'+(String.fromCharCode(i+97)).toUpperCase()+'</b></td>');
+  }
+  return ['<td><b>'+name+"</b></td>"].concat(row);
+}
+
+function getNoofRowsAndCols(data){
+  
+  let cols=[];let rows=[];
+  for(const i of data){cols.push(i[0]);}
+  for(const j of data){rows.push(j.substr(1));}
+  if(data.length===0){return [MIN_ROWS,MIN_COLS]}
+  cols.sort();rows.sort();
+  const rowNo=parseInt(rows[rows.length-1]-1)>MIN_ROWS?parseInt(rows[rows.length-1]-1):MIN_ROWS;
+  const colNo=cols[cols.length-1].charCodeAt(0)-96>MIN_COLS?cols[cols.length-1].charCodeAt(0)-96:MIN_COLS;
+  return [rowNo,colNo];
+  }
 
 /**************************** Validation ********************************/
 
@@ -117,10 +207,13 @@ const FIELD_INFOS = {
  *  message as errors[name].
  */
 function validateField(name, params, errors) {
+  
   const info = FIELD_INFOS[name];
   const value = params[name];
+  
   if (isEmpty(value)) {
     errors[name] = `The ${info.friendlyName} field must be specified`;
+  
     return false;
   }
   if (info.err) {
@@ -186,7 +279,12 @@ function validateFields(act, required, forbidden, params, errors) {
 
 
 /************************ General Utilities ****************************/
-
+/** these are to map out errors thwn by web services G */
+function wsErrors(err) {
+  const msg = (err.message) ? err.message : 'web service error';
+  console.error(msg);
+  return { _: [ msg ] };
+}
 /** return new object just like paramsObj except that all values are
  *  trim()'d.
  */
